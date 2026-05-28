a076491ae554d0ddc6884c5f69fe215f50f0743f
AURYSS <ceciaurora2017@gmail.com>
2026-05-27 23:24:54 -0600

    Implementa funcionalidades de la sesión 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,5 +59,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("com.google.android.gms:play-services-wearable:18.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3&quot")
 
 }
